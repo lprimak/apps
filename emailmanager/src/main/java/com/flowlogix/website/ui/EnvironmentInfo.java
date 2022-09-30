60166f3911cec00dfed4555d27b5f38ae58295d6
lprimak <lenny@flowlogix.com>
2022-09-29 20:31:26 -0500

    added version info

---
+++ emailmanager/src/main/java/com/flowlogix/website/ui/EnvironmentInfo.java
@@ -0,0 +1,42 @@
+package com.flowlogix.website.ui;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ *
+ * @author lprimak
+ */
+@Named
+@ApplicationScoped
+public class EnvironmentInfo {
+    private final String version;
+    private final String gitId;
+    private final String buildTime;
+
+    public EnvironmentInfo() {
+        Properties props = new Properties();
+        try ( InputStream is = getClass().getClassLoader().getResourceAsStream("git.properties")) {
+            props.load(new BufferedInputStream(is));
+        } catch (IOException ex) {
+            ExceptionUtils.rethrow(ex);
+        }
+
+        version = props.getProperty("git.build.version");
+        gitId = props.getProperty("git.commit.id.abbrev");
+        buildTime = props.getProperty("git.build.time");
+    }
+
+    public String getVersion() {
+        return String.join("-", version, gitId);
+    }
+
+    public String getBuildTime() {
+        return buildTime;
+    }
+}
