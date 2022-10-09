f6e72142185ee578f27b09e43bfe3686664b18f9
lprimak <lenny@flowlogix.com>
2022-10-08 21:13:45 -0500

    printing out error when pinging

--- emailmanager/src/main/java/com/flowlogix/website/ui/EmailManagerProducer.java
+++ emailmanager/src/main/java/com/flowlogix/website/ui/EmailManagerProducer.java
@@ -3,6 +3,7 @@ package com.flowlogix.website.ui;
 import com.flowlogix.website.EmailManagerLocal;
 import javax.enterprise.context.ApplicationScoped;
 import javax.mail.MessagingException;
+import lombok.extern.slf4j.Slf4j;
 import org.omnifaces.util.JNDIObjectLocator;
 import org.omnifaces.util.Lazy;
 
@@ -11,6 +12,7 @@ import org.omnifaces.util.Lazy;
  * @author lprimak
  */
 @ApplicationScoped
+@Slf4j
 public class EmailManagerProducer {
     private final JNDIObjectLocator locator = JNDIObjectLocator.builder().build();
     private final Lazy<EmailManagerLocal> emailManager = new Lazy<>(this::createEmailManager);
@@ -25,6 +27,7 @@ public class EmailManagerProducer {
             eraserImpl.pingImap();
             return eraserImpl;
         } catch (MessagingException e) {
+            log.warn("Ping Imap", e);
             return locator.getObject("java:module/EmailManagerMock");
         }
     }
