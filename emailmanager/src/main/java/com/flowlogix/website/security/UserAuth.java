315f97dd8fa6f1214af7cf79ca1300e17bb9e519
lprimak <lenny@flowlogix.com>
2022-11-17 14:25:10 -0600

    encrypting username and passwords in UserAuth class

--- emailmanager/src/main/java/com/flowlogix/website/security/UserAuth.java
+++ emailmanager/src/main/java/com/flowlogix/website/security/UserAuth.java
@@ -4,20 +4,53 @@
  */
 package com.flowlogix.website.security;
 
+import com.flowlogix.shiro.ee.cdi.KeyGen.CipherKeySupplier;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.apache.shiro.crypto.AesCipherService;
+import org.omnifaces.util.Beans;
+import org.omnifaces.util.Lazy;
 
 /**
  *
  * @author lprimak
  */
-@Getter
 @EqualsAndHashCode
-@RequiredArgsConstructor
 public class UserAuth implements Serializable {
+    private final byte[] userName;
+    private final byte[] password;
+
     private static final long serialVersionUID = 1L;
-    private final String userName;
-    private final String password;
+    @SuppressWarnings("serial")
+    private final Lazy<CipherKeySupplier> keySource
+            = new Lazy<>(() -> Beans.getReference(CipherKeySupplier.class));
+    @SuppressWarnings("serial")
+    private final Lazy<AesCipherService> cipherService = new Lazy<>(AesCipherService::new);
+
+    public UserAuth(String userName, String password) {
+        this.userName = cipherService.get().encrypt(userName.getBytes(StandardCharsets.UTF_8),
+                keySource.get().get().getBytes(StandardCharsets.UTF_8)).getBytes();
+        this.password = cipherService.get().encrypt(password.getBytes(StandardCharsets.UTF_8),
+                keySource.get().get().getBytes(StandardCharsets.UTF_8)).getBytes();
+    }
+
+    private UserAuth(byte[] userName, byte[] password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return new String(cipherService.get().decrypt(userName, keySource.get().get()
+                .getBytes(StandardCharsets.UTF_8)).getBytes(), StandardCharsets.UTF_8);
+    }
+
+    public String getPassword() {
+        return new String(cipherService.get().decrypt(password, keySource.get().get()
+                .getBytes(StandardCharsets.UTF_8)).getBytes(), StandardCharsets.UTF_8);
+    }
+
+    protected Object readResolve() {
+        return new UserAuth(userName, password);
+    }
 }
