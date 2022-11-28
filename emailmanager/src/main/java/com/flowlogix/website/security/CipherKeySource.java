ee68530dfae23403dc43eb6d2efa59826623d095
lprimak <lenny@flowlogix.com>
2022-11-27 20:38:41 -0500

    - packaging own libpam4j and JNI, instead of relying on Payara - works with a unconfigured cipher key as well

--- emailmanager/src/main/java/com/flowlogix/website/security/CipherKeySource.java
+++ emailmanager/src/main/java/com/flowlogix/website/security/CipherKeySource.java
@@ -2,8 +2,12 @@ package com.flowlogix.website.security;
 
 import com.flowlogix.shiro.ee.cdi.KeyGen.CipherKeySupplier;
 import com.flowlogix.website.ui.Constants;
+import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import org.apache.shiro.crypto.AesCipherService;
 
 /**
  *
@@ -11,11 +15,23 @@ import javax.inject.Inject;
  */
 @ApplicationScoped
 public class CipherKeySource implements CipherKeySupplier {
+    private String cipherKey;
+
     @Inject
     Constants constants;
 
+    @PostConstruct
+    void init() {
+        cipherKey = constants.getCipherKey();
+        if (isBlank(cipherKey)) {
+            var acs = new AesCipherService();
+            cipherKey = new String(acs.generateNewKey(128)
+                    .getEncoded(), StandardCharsets.UTF_8);
+        }
+    }
+
     @Override
     public String get() {
-        return constants.getCipherKey();
+        return cipherKey;
     }
 }
