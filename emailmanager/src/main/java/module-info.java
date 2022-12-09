37b4f0ebe3ada479ffa452baa55046bb824129a2
lprimak <lenny@flowlogix.com>
2022-12-09 14:16:32 -0600

    JDK module support

---
+++ emailmanager/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module HopeNYC {
+    requires jakarta.jakartaee.api;
+    requires lombok;
+    requires shiro.core;
+    requires omnifaces;
+    requires primefaces;
+    requires microprofile.config.api;
+    requires flowlogix.datamodel;
+    requires flowlogix.jee;
+    requires org.apache.commons.lang3;
+    requires shiro.ee;
+    requires shiro.crypto.cipher;
+    requires shiro.crypto.core;
+    requires shiro.lang;
+    requires libpam4j;
+    requires org.slf4j;
+}
