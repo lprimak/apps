3d9d59f9ef81cfbf21984e0bebbd9a01a9564040
lprimak <lenny@flowlogix.com>
2023-06-29 23:51:39 -0500

    deps: update to Shiro alpha-3 module names

--- emailmanager/src/main/java/module-info.java
+++ emailmanager/src/main/java/module-info.java
@@ -5,18 +5,18 @@ module HopeNYC {
     requires jakarta.validation;
     requires jakarta.faces.api;
     requires lombok;
-    requires shiro.core;
     requires org.omnifaces;
     requires primefaces;
     requires microprofile.config.api;
     requires com.flowlogix.datamodel;
     requires com.flowlogix.jee;
     requires org.apache.commons.lang3;
-    requires shiro.jakarta.ee;
-    requires shiro.cdi;
-    requires shiro.crypto.cipher;
-    requires shiro.crypto.core;
-    requires shiro.lang;
+    requires org.apache.shiro.core;
+    requires org.apache.shiro.jakarta.ee;
+    requires org.apache.shiro.cdi;
+    requires org.apache.shiro.crypto.cipher;
+    requires org.apache.shiro.crypto.core;
+    requires org.apache.shiro.lang;
     requires libpam4j;
     requires org.slf4j;
 }
