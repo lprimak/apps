1467ba20199093b74a3225ff7fbd801f20581a5b
lprimak <lenny@flowlogix.com>
2023-09-17 00:26:12 -0500

    bugfix: put back @GeneratedValue - it's required

--- emailmanager/src/main/java/com/flowlogix/website/entities/Sample.java
+++ emailmanager/src/main/java/com/flowlogix/website/entities/Sample.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQuery;
 import jakarta.validation.constraints.Size;
@@ -24,6 +25,7 @@ import lombok.Data;
 public class Sample implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue
     private Long id;
 
     @Size(max = 300)
